287072a19e60bd5371f37dc1cd859d272b749520
ahcgnoej <jinjinchachajung@naver.com>
2024-11-21 15:06:56 +0900

    chore: css setting

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,22 @@
 module.exports = {
   content: [
     "./index.html", 
-    "./src/**/*.{js,ts,jsx,tsx}" // 파일 확장자에 따라 수정
+    "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Noto Sans KR'], 
+        title: ['Righteous'],
+      },
+      colors: {
+        darkBlue: '#1A2A6C',  // 진한 블루 (버튼 테두리, 문장 포인트)
+        lightBlue: '#68B8FF', // 연한 블루 (단어 포인트)
+        darkGray: '#787878',  // 진한 그레이 (텍스트 색상)
+        lightGray: '#EEEEEE', // 연한 그레이 (배경 색상)
+        black: '#393939',     // 검정색
+      },
+    },
   },
   plugins: [],
 };
