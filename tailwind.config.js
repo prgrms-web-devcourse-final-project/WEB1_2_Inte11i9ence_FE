c7f01327ca3bb6086c60a9f37748172b2d2a8a41
ahcgnoej <jinjinchachajung@naver.com>
2024-11-21 12:17:33 +0900

    chore: add Tailwind CSS setting

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html", 
+    "./src/**/*.{js,ts,jsx,tsx}" // 파일 확장자에 따라 수정
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
