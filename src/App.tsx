bb5254d78771cf3cc6b894c5ac82047b34547dd7
ahcgnoej <jinjinchachajung@naver.com>
2024-11-23 00:45:06 +0900

    Feat: 라우터 연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import './App.css'
+import Navigation from './pages/Navigater/Navigater'
+import SchedulePage from './pages/SchedulePage/SchedulePage'
+import { Routes, Route } from 'react-router-dom'
 import Container from './layouts/bodyContainer'
-
 const App = () => {
   return (
-    <Container>
-    <div className="h-full">
-      <h1>홧팅</h1>
+    <div>
+      <Container>
+        <Navigation />
+        <Routes>
+          <Route
+            path='/schedule'
+            element={<SchedulePage />}
+          ></Route>
+        </Routes>
+      </Container>
     </div>
-    </Container>
   )
 }
 
