7dca0f4b5d7b8cf26abd42ce4de90e31512ecab9
ahcgnoej <jinjinchachajung@naver.com>
2024-11-27 12:14:29 +0900

    Feat: 사진 게시판 마크업

---
+++ src/pages/PhotoPage/mockdata.ts
@@ -0,0 +1,130 @@
+export const photoList = [
+  {
+    type: '인물',
+    regionName: '서울',
+    content:
+      '어디어디 놀러가서 찍은 첫 번째 사진이랑 두 번째 사진 중에 뭐가 더 잘 나왔는지 투표해주세요ㅠㅠㅠ',
+    date: '2023-11-01',
+    tags: ['태그1', '태그2', '태그3'],
+    ImgUrl:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoV1w-qvi0_hmEVu_uemLxtPglSevwCJLa-A&s',
+  },
+  {
+    type: '배경',
+    regionName: '서울',
+    content: '해운대에서 찍은 사진인데 어떤가요?',
+    date: '2023-11-02',
+    tags: ['바다', '여행', '추억'],
+    ImgUrl:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoV1w-qvi0_hmEVu_uemLxtPglSevwCJLa-A&s',
+  },
+  {
+    type: '인물',
+    regionName: '서울',
+    content: '제주도에서 찍은 제 모습! 분위기 괜찮나요?',
+    date: '2023-11-03',
+    tags: ['제주도', '셀카', '힐링'],
+    ImgUrl:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoV1w-qvi0_hmEVu_uemLxtPglSevwCJLa-A&s',
+  },
+  {
+    type: '배경',
+    regionName: '서울',
+    content: '대구의 야경이 멋져서 한 컷 찍었습니다!',
+    date: '2023-11-04',
+    tags: ['야경', '도시', '야경사진'],
+    ImgUrl:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoV1w-qvi0_hmEVu_uemLxtPglSevwCJLa-A&s',
+  },
+  {
+    type: '인물',
+    regionName: '서울',
+    content: '친구랑 광주에서 찍은 사진입니다. 잘 나왔나요?',
+    date: '2023-11-05',
+    tags: ['친구', '광주', '인물사진'],
+    ImgUrl:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoV1w-qvi0_hmEVu_uemLxtPglSevwCJLa-A&s',
+  },
+  {
+    type: '배경',
+    regionName: '서울',
+    content: '인천공항 근처에서 본 멋진 풍경이에요.',
+    date: '2023-11-06',
+    tags: ['풍경', '공항', '여행'],
+    ImgUrl:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoV1w-qvi0_hmEVu_uemLxtPglSevwCJLa-A&s',
+  },
+  {
+    type: '인물',
+    regionName: '서울',
+    content: '강릉의 아름다운 해변에서 찍은 제 사진입니다!',
+    date: '2023-11-07',
+    tags: ['해변', '셀카', '강릉'],
+    ImgUrl:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoV1w-qvi0_hmEVu_uemLxtPglSevwCJLa-A&s',
+  },
+  {
+    type: '인물',
+    regionName: '서울',
+    content:
+      '어디어디 놀러가서 찍은 첫 번째 사진이랑 두 번째 사진 중에 뭐가 더 잘 나왔는지 투표해주세요ㅠㅠㅠ',
+    date: '2023-11-01',
+    tags: ['태그1', '태그2', '태그3'],
+    ImgUrl:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoV1w-qvi0_hmEVu_uemLxtPglSevwCJLa-A&s',
+  },
+  {
+    type: '배경',
+    regionName: '서울',
+    content: '해운대에서 찍은 사진인데 어떤가요?',
+    date: '2023-11-02',
+    tags: ['바다', '여행', '추억'],
+    ImgUrl:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoV1w-qvi0_hmEVu_uemLxtPglSevwCJLa-A&s',
+  },
+  {
+    type: '인물',
+    regionName: '서울',
+    content: '제주도에서 찍은 제 모습! 분위기 괜찮나요?',
+    date: '2023-11-03',
+    tags: ['제주도', '셀카', '힐링'],
+    ImgUrl:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoV1w-qvi0_hmEVu_uemLxtPglSevwCJLa-A&s',
+  },
+  {
+    type: '배경',
+    regionName: '서울',
+    content: '대구의 야경이 멋져서 한 컷 찍었습니다!',
+    date: '2023-11-04',
+    tags: ['야경', '도시', '야경사진'],
+    ImgUrl:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoV1w-qvi0_hmEVu_uemLxtPglSevwCJLa-A&s',
+  },
+  {
+    type: '인물',
+    regionName: '서울',
+    content: '친구랑 광주에서 찍은 사진입니다. 잘 나왔나요?',
+    date: '2023-11-05',
+    tags: ['친구', '광주', '인물사진'],
+    ImgUrl:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoV1w-qvi0_hmEVu_uemLxtPglSevwCJLa-A&s',
+  },
+  {
+    type: '배경',
+    regionName: '서울',
+    content: '인천공항 근처에서 본 멋진 풍경이에요.',
+    date: '2023-11-06',
+    tags: ['풍경', '공항', '여행'],
+    ImgUrl:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoV1w-qvi0_hmEVu_uemLxtPglSevwCJLa-A&s',
+  },
+  {
+    type: '인물',
+    regionName: '서울',
+    content: '강릉의 아름다운 해변에서 찍은 제 사진입니다!',
+    date: '2023-11-07',
+    tags: ['해변', '셀카', '강릉'],
+    ImgUrl:
+      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRoV1w-qvi0_hmEVu_uemLxtPglSevwCJLa-A&s',
+  },
+]
