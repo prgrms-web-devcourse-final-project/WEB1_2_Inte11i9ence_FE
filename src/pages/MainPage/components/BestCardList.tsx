e598905ecb3df1605ec6b5257d0ec15c939904e8
jiwonkim <kiwi_dev_@naver.com>
2024-11-28 14:13:58 +0900

    Style: 메인페이지 내 컴포넌트 크기 관련 css 수정

--- src/pages/MainPage/components/BestCardList.tsx
+++ src/pages/MainPage/components/BestCardList.tsx
@@ -5,14 +5,17 @@ import { postData } from '@/temporaryData/allPostData';
 const BestCardList = () => {
     const bestPosts = postData.sort((a,b) => b.likes - a.likes).slice(0, 3);
     return (
-        <div className="flex flex-col w-full my-24">
-            <div className='flex flex-row justify-center w-full'>
-                <div className='mb-4 flex flex-row justify-between w-[calc(24rem+3rem)]'>
-                    <span className='text-lg font-bold'>금주의 인기 포스트</span>
-                    <Link to='/bestpage' className='text-darkGray'>더보기</Link>
-                </div>
-            </div>
-            <div className='flex flex-col space-y-4 mx-12 w-[calc(26rem+7rem)]'>
+        <div className="flex flex-col w-full my-26">
+            <header className='flex justify-between items-center mb-4 px-10 w-full'>
+                <h2 className='text-[1.5rem] font-bold'>금주의 인기 포스트</h2>
+                <Link 
+                    to='/bestpage' 
+                    className='text-darkGray hover:text-black'
+                >
+                    더보기
+                </Link>
+            </header>
+            <div className='flex flex-col gap-12 my-8 mx-12 '>
       {bestPosts.map((post) => (
         <BestCardItem key={post.id} post={post} />
       ))}
