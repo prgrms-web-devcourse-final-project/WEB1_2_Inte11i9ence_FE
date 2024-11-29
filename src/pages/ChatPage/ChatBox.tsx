2b018887fbadb0fb8b702f5f155abaaeea3d600e
ahcgnoej <jinjinchachajung@naver.com>
2024-11-30 03:48:26 +0900

    Feat: 채팅 기능 웹소켓 부분 임의추가 후 주석 처리

--- src/pages/ChatPage/ChatBox.tsx
+++ src/pages/ChatPage/ChatBox.tsx
@@ -1,95 +1,104 @@
-import { useEffect, useRef } from 'react'
-import ScrollableContainer from '@/components/Scroll'
-const messages = [
-  { id: 1, sender: 'user', text: '안녕하세요!', timestamp: '10:00 AM' },
-  {
-    id: 2,
-    sender: 'other',
-    text: '안녕하세요! 반갑습니다.',
-    timestamp: '10:01 AM',
-  },
-  { id: 3, sender: 'user', text: '모하세요', timestamp: '10:02 AM' },
-  { id: 4, sender: 'other', text: '그냥있어요', timestamp: '10:03 AM' },
-  { id: 5, sender: 'user', text: '안녕하세요!', timestamp: '10:03 AM' },
-  {
-    id: 6,
-    sender: 'other',
-    text: '안녕하세요! 반갑습니다.',
-    timestamp: '10:04 AM',
-  },
-  { id: 7, sender: 'user', text: '모하세요?', timestamp: '10:05 AM' },
-  { id: 8, sender: 'other', text: '그냥있어요!', timestamp: '10:06 AM' },
-]
+// import { useEffect, useRef, useState } from 'react'
+// import ScrollableContainer from '@/components/Scroll'
+// import { messages } from './mockdata'
+// import axios from 'axios'
+// import defaultProfileImage from '@assets/png/default-profile-2.png'
 
-const ChatBox = () => {
-  // 스크롤을 관리할 ref 생성, 스크롤 맨 아래 이동에 사용ㅇ
-  const scrollContainerRef = useRef<HTMLDivElement>(null)
-  useEffect(() => {
-    if (scrollContainerRef.current) {
-      scrollContainerRef.current.scrollTop =
-        scrollContainerRef.current.scrollHeight
-    }
-  }, [])
-  useEffect(() => {
-    if (scrollContainerRef.current) {
-      scrollContainerRef.current.scrollTop =
-        scrollContainerRef.current.scrollHeight
-    }
-  }, [messages])
+// const ChatBox = ({ socket, chatRoomId, messages, setMessages }) => {
+//   const [newMessage, setNewMessage] = useState('')
+//   // 스크롤을 관리할 ref 생성, 스크롤 맨 아래 이동에 사용ㅇ
+//   const scrollContainerRef = useRef<HTMLDivElement>(null)
+//   useEffect(() => {
+//     if (scrollContainerRef.current) {
+//       scrollContainerRef.current.scrollTop =
+//         scrollContainerRef.current.scrollHeight
+//     }
+//   }, [])
+//   useEffect(() => {
+//     if (scrollContainerRef.current) {
+//       scrollContainerRef.current.scrollTop =
+//         scrollContainerRef.current.scrollHeight
+//     }
+//   }, [messages])
 
-  return (
-    <div className='flex flex-col h-[70vh] w-full mx-auto bg-white border shadow'>
-      <div className='flex items-center justify-between bg-lightGrays px-4 py-2 border-b'>
-        <div className='flex items-center gap-2'>
-          <div className='rounded-full bg-gray-300 w-10 h-10'></div>
-          <p className='font-semibold text-black'>채팅 상대 이름</p>
-        </div>
-      </div>
-      <ScrollableContainer
-        ref={scrollContainerRef}
-        style={{ height: 'calc(100% - 90px)', overflowY: 'auto' }}
-      >
-        <div className='flex flex-col flex-1 p-4 gap-4'>
-          {messages.map((msg) => (
-            <div
-              key={msg.id}
-              className={`flex ${msg.sender === 'user' ? 'justify-end' : 'justify-start'}`}
-            >
-              <div>
-                <div
-                  className={`px-4 py-2 rounded-lg text-[13px] ${
-                    msg.sender === 'user'
-                      ? 'bg-darkBlue text-white'
-                      : 'bg-gray-200 text-black'
-                  }`}
-                >
-                  <p>{msg.text}</p>
-                </div>
-                <span
-                  className={`text-xs mt-1 block ${
-                    msg.sender === 'user' ? 'text-right' : 'text-left'
-                  }`}
-                >
-                  {msg.timestamp}
-                </span>
-              </div>
-            </div>
-          ))}
-        </div>
-      </ScrollableContainer>
+//   // 메시지 전송
+//   const handleSendMessage = async () => {
+//     if (newMessage.trim() && socket) {
+//       const messageData = {
+//         content: newMessage,
+//         sender: messages.sender, // 실제 사용자 정보를 사용하도록 변경
+//         sentAt: new Date().toISOString(),
+//       }
 
-      <div className='flex items-center gap-2 p-4 border-t bg-gray-50'>
-        <input
-          type='text'
-          placeholder='메시지를 입력하세요...'
-          className='flex-1 px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-darkBlue'
-        />
-        <button className='px-4 py-2 bg-darkBlue text-white rounded-lg hover:bg-blue-600'>
-          전송
-        </button>
-      </div>
-    </div>
-  )
-}
+//       socket.send(JSON.stringify(messageData)) // 웹소켓 전송
+//       setMessages((prev) => [...prev, messageData]) // 로컬 상태 업데이트
+//       setNewMessage('')
+//     }
+//   }
 
-export default ChatBox
+//   return (
+//     <div className='flex flex-col h-[70vh] w-full mx-auto bg-white border shadow'>
+//       <div className='flex items-center justify-between bg-lightGrays px-4 py-2 border-b'>
+//         <div className='flex items-center gap-2'>
+//           <div className='w-8 h-8 rounded-full overflow-hidden'>
+//             <img
+//               src={messages.profileImage || defaultProfileImage}
+//               alt='Profile'
+//               className='w-full h-full object-cover'
+//             />
+//           </div>
+//           <p className='font-semibold text-black'>{messages.sender}</p>
+//         </div>
+//       </div>
+//       <ScrollableContainer
+//         ref={scrollContainerRef}
+//         style={{ height: 'calc(100% - 90px)', overflowY: 'auto' }}
+//       >
+//         <div className='flex flex-col flex-1 p-4 gap-4'>
+//           {messages.map((msg) => (
+//             <div
+//               key={msg.id}
+//               className={`flex ${msg.sender === 'user' ? 'justify-end' : 'justify-start'}`}
+//             >
+//               <div>
+//                 <div
+//                   className={`px-4 py-2 rounded-lg text-[13px] ${
+//                     msg.sender === 'user'
+//                       ? 'bg-darkBlue text-white'
+//                       : 'bg-gray-200 text-black'
+//                   }`}
+//                 >
+//                   <p>{msg.content}</p>
+//                 </div>
+//                 <span
+//                   className={`text-xs mt-1 block ${
+//                     msg.sender === 'user' ? 'text-right' : 'text-left'
+//                   }`}
+//                 >
+//                   {msg.sentAt}
+//                 </span>
+//               </div>
+//             </div>
+//           ))}
+//         </div>
+//       </ScrollableContainer>
+
+//       <div className='flex items-center gap-2 p-4 border-t bg-gray-50'>
+//         <input
+//           type='text'
+//           placeholder='메시지를 입력하세요...'
+//           className='flex-1 px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-darkBlue'
+//           onChange={(e) => setNewMessage(e.target.value)}
+//         />
+//         <button
+//           className='px-4 py-2 bg-darkBlue text-white rounded-lg hover:bg-blue-600'
+//           onClick={handleSendMessage}
+//         >
+//           전송
+//         </button>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default ChatBox
--- src/pages/ChatPage/ChatPage.tsx
+++ src/pages/ChatPage/ChatPage.tsx
@@ -1,87 +1,121 @@
 import defaultProfileImage from '@assets/png/default-profile-2.png'
 import ChatBox from './ChatBox'
 import ScrollableContainer from '@/components/Scroll'
+import { chats } from './mockdata'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 const ChatPage = () => {
-  const chats = [
-    {
-      id: 1,
-      title: '지역게시판의 어떤 어떤 이거 저거',
-      nickname: '닉네임1',
-    },
-    { id: 2, title: '채팅방2', nickname: '닉네임2' },
-    {
-      id: 1,
-      title: '지역게시판의 어떤 어떤 이거 저거',
-      nickname: '닉네임1',
-    },
-    { id: 2, title: '채팅방2', nickname: '닉네임2' },
-    {
-      id: 1,
-      title: '지역게시판의 어떤 어떤 이거 저거',
-      nickname: '닉네임1',
-    },
-    { id: 2, title: '채팅방2', nickname: '닉네임2' },
-    {
-      id: 1,
-      title: '지역게시판의 어떤 어떤 이거 저거',
-      nickname: '닉네임1',
-    },
-    { id: 2, title: '채팅방2', nickname: '닉네임2' },
-    {
-      id: 1,
-      title: '지역게시판의 어떤 어떤 이거 저거',
-      nickname: '닉네임1',
-    },
-    { id: 2, title: '채팅방2', nickname: '닉네임2' },
-    {
-      id: 1,
-      title: '지역게시판의 어떤 어떤 이거 저거',
-      nickname: '닉네임1',
-    },
-    { id: 2, title: '채팅방2', nickname: '닉네임2' },
-  ]
-  return (
-    <div className='flex justify-center items-center w-full'>
-      <div className='flex w-[80vw] h-[70vh] items-center justify-center gap-16'>
-        <div className='flex flex-col flex-[4] h-full gap-6 justify-start items-start'>
-          <div>
-            <p>
-              DM <span className='font-bold'>3</span>개
-            </p>
-          </div>
-          <ScrollableContainer style={{ height: 'calc(100% - 40px)' }}>
-            <div className='flex flex-col gap-4'>
-              {chats.map((chat) => (
-                <button
-                  key={chat.id}
-                  className='flex border-b pb-3  w-full'
-                >
-                  <div className='w-8 h-8 rounded-full overflow-hidden'>
-                    <img
-                      src={defaultProfileImage}
-                      alt='Profile'
-                      className='w-full h-full object-cover'
-                    />
-                  </div>{' '}
-                  <div className='ml-4'>
-                    <p className='flex items-start text-sm font-bold'>
-                      {chat.title}
-                    </p>
-                    <p className=' flex items-start text-xs text-darkGray truncate'>
-                      {chat.nickname}
-                    </p>
-                  </div>
-                </button>
-              ))}
-            </div>
-          </ScrollableContainer>
-        </div>
-        <div className='flex-[7]'>
-          <ChatBox />
-        </div>
-      </div>
-    </div>
-  )
+  // const [chatList, setChatList] = useState([])
+  // const [currentChatRoomId, setCurrentChatRoomId] = useState<number | null>(
+  //   null,
+  // )
+  // const [messages, setMessages] = useState([])
+  // const [socket, setSocket] = useState<WebSocket | null>(null)
+  // //채팅방 목록 가져오기
+  // useEffect(() => {
+  //   const fetchChats = async () => {
+  //     try {
+  //       const response = await axios.get('api/v1/chat/chatRoom')
+  //       setChatList(response.data)
+  //     } catch (error) {
+  //       console.error('채팅방 목록을 가져오는 데 실패했습니다', error)
+  //     }
+  //   }
+  //   fetchChats()
+  // }, [])
+  // // 선택된 채팅방 메시지 가져오기
+  // useEffect(() => {
+  //   if (currentChatRoomId) {
+  //     const fetchMessages = async () => {
+  //       try {
+  //         const response = await axios.get(`/api/v1/chat/${currentChatRoomId}`)
+  //         setMessages(response.data)
+  //       } catch (error) {
+  //         console.error('채팅 메시지를 가져오는 데 실패했습니다', error)
+  //       }
+  //     }
+  //     fetchMessages()
+  //   }
+  // }, [currentChatRoomId])
+  // // 웹소켓 연결
+  // useEffect(() => {
+  //   if (currentChatRoomId) {
+  //     const socket = new WebSocket(
+  //       `ws://localhost:8080/chat-room/${currentChatRoomId}`,
+  //     )
+  //     socket.onopen = () => {
+  //       console.log('웹소켓 연결 성공')
+  //     }
+  //     socket.onmessage = (event) => {
+  //       const message = JSON.parse(event.data)
+  //       // 새로운 메시지를 받은 후 처리 (ChatBox에 전달 등)
+  //       setMessages((prevMessages) => [...prevMessages, message]) // 실시간 메시지 추가
+  //     }
+  //     socket.onclose = () => {
+  //       console.log('웹소켓 연결 종료')
+  //     }
+  //     setSocket(socket)
+  //     return () => {
+  //       if (socket) {
+  //         socket.close()
+  //       }
+  //     }
+  //   }
+  // }, [currentChatRoomId])
+  // //채팅방 클릭 시 해당 방의 채팅 열림
+  // const handleChatRoomClick = (currentChatRoomId: number) => {
+  //   setCurrentChatRoomId(currentChatRoomId)
+  // }
+  // return (
+  //   <div className='flex justify-center items-center w-full'>
+  //     <div className='flex w-[80vw] h-[70vh] items-center justify-center gap-16'>
+  //       <div className='flex flex-col flex-[4] h-full gap-6 justify-start items-start'>
+  //         <div>
+  //           <p>
+  //             DM <span className='font-bold'>{chatList.length}</span>개
+  //           </p>
+  //         </div>
+  //         <ScrollableContainer style={{ height: 'calc(100% - 40px)' }}>
+  //           <div className='flex flex-col gap-4'>
+  //             {chatList.map((chat) => (
+  //               <button
+  //                 key={chat.id}
+  //                 className='flex border-b pb-3  w-full'
+  //                 onClick={() => handleChatRoomClick(chat.id)}
+  //               >
+  //                 <div className='w-8 h-8 rounded-full overflow-hidden'>
+  //                   <img
+  //                     src={chat.profileImage || defaultProfileImage}
+  //                     alt='Profile'
+  //                     className='w-full h-full object-cover'
+  //                   />
+  //                 </div>{' '}
+  //                 <div className='ml-4'>
+  //                   <p className='flex items-start text-sm font-bold'>
+  //                     {chat.content}
+  //                   </p>
+  //                   <p className=' flex items-start text-xs text-darkGray truncate'>
+  //                     {chat.nickname}
+  //                   </p>
+  //                 </div>
+  //               </button>
+  //             ))}
+  //           </div>
+  //         </ScrollableContainer>
+  //       </div>
+  //       <div className='flex-[7]'>
+  //         {currentChatRoomId && (
+  //           <ChatBox
+  //             socket={socket}
+  //             chatRoomId={currentChatRoomId}
+  //             messages={messages}
+  //             setMessages={setMessages}
+  //           />
+  //         )}
+  //       </div>
+  //     </div>
+  //   </div>
+  // )
 }
 export default ChatPage
