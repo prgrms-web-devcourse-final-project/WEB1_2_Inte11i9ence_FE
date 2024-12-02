462dbc0249df4445e0bcb83c125c2704c23f4bd4
jiwonkim <kiwi_dev_@naver.com>
2024-12-02 18:41:50 +0900

    Feat: 지역게시판 컴포넌트 세부 분리, 카테고리 색상 지정, 정렬 드롭다운, 제공지명, 별점 데이터 추가

--- src/pages/PostListPage/PostItem.tsx
+++ src/pages/PostListPage/PostItem.tsx
@@ -9,6 +9,21 @@ interface PostItemProps {
 }
 
 const PostItem = ({ post }: PostItemProps) => {
+
+    // 카테고리별 색상 지정
+    const getCategoryColor = (category: string) => {
+        switch(category) {
+            case '자유':
+                return 'text-lightBlue border-lightBlue';
+            case '리뷰':
+                return 'text-yellow-500 border-yellow-500';
+            case '공지':
+                return 'text-pink-500 border-pink-500';
+            default:
+                return 'text-darkBlue border-darkBlue';
+        }
+    };
+
     return (
         <div>
             <div className="flex justify-center w-full">
@@ -19,7 +34,11 @@ const PostItem = ({ post }: PostItemProps) => {
                 <div className='flex flex-col justify-around w-[48rem] h-[10rem] px-8 py-4 mr-4'>
                     <div className='flex flex-col space-y-3'>
                         <div className='flex'>
-                            <span className='border-2 border-lightBlue rounded-xl text-lightBlue mx-2 px-1 mt-0.5'>{post.category}</span>
+                            {post.category === '리뷰'?
+                            (<span className={`border-2 rounded-xl mx-2 px-1 mt-0.5 ${getCategoryColor(post.category)}`}>{post.rating}</span>)
+                            :(<span className={`border-2 rounded-xl mx-2 px-1 mt-0.5 ${getCategoryColor(post.category)}`}>{post.category}</span>)
+                            }
+                             {/* <span className={`border-2 rounded-xl mx-2 px-1 mt-0.5 ${getCategoryColor(post.category)}`}>{post.category}</span> */}
                         <span className='text-2xl font-bold'>{post.title}</span>
                         </div>
                         <span className='text-ml text-left text-darkGray overflow-hidden h-[3.2em] line-clamp-2'>{post.content}</span>
--- src/pages/PostListPage/PostListPage.tsx
+++ src/pages/PostListPage/PostListPage.tsx
@@ -4,26 +4,33 @@ import { useState } from 'react';
 import { postData } from '@/temporaryData/allPostData';
 import DropdownSelector from '@/components/DropdownSelector';
 import RegionDropdown from '@/components/RegionDropdown';
+import { useMemo } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const PostListPage = () => {
     const [selectedCategory, setSelectedCategory] = useState('전체')
     const [selectedDetailCategory, setSelectedDetailCategory] = useState('지역 전체')
     const [isDetailCategoryOpen, setIsDetailCategoryOpen] = useState(false)
+    const [sortType, setSortType] = useState('최신순');
+    const navigate = useNavigate();
 
     const handleRegionCategory = (selected: string) => {
         setSelectedCategory(selected);
-        // 지역이 아닌 다른 카테고리 선택시 지역 카테고리 비활성화
-        if (selected !== '지역') {
+
+        // '지역' 카테고리 선택 시 상세 지역 선택 드롭다운 표시 및 postlist/region 라우터로 이동
+        if (selected === '지역') {
+            setIsDetailCategoryOpen(true);
+            navigate('/postlist/region');
+            console.log(isDetailCategoryOpen)
+        } else {
             setIsDetailCategoryOpen(false);
             setSelectedDetailCategory('지역 전체');
-        } else {
-            setIsDetailCategoryOpen(true);
+            navigate('/postlist');
         }
     };
 
     const options = [
         { value: '전체', label: '전체' },
-        // { value: '공지', label: '공지' },
         { value: '자유', label: '자유' },
         { value: '지역', label: '지역' },
         { value: '리뷰', label: '리뷰' },
@@ -31,19 +38,37 @@ const PostListPage = () => {
 
     const regionOptions = [
         { value: '지역 전체', label: '지역 전체' },
-        { value: '서울', label: '서울' },
+        { value: '강원도', label: '강원도' },
+        { value: '경기도', label: '경기도' },
+        { value: '경상남도', label: '경상남도' },
+        { value: '경상북도', label: '경상북도' },
+        { value: '광주', label: '광주' },
+        { value: '대구', label: '대구' },
+        { value: '대전', label: '대전' },
         { value: '부산', label: '부산' },
-        { value: '제주', label: '제주' },
-        { value: '강릉', label: '강릉' },
+        { value: '서울', label: '서울' },
+        { value: '세종', label: '세종' },
+        { value: '울산', label: '울산' },
+        { value: '인천', label: '인천' },
+        { value: '전라남도', label: '전라남도' },
+        { value: '전라북도', label: '전라북도' },
+        { value: '제주도', label: '제주도' },
+        { value: '충청남도', label: '충청남도' },
+        { value: '충청북도', label: '충청북도' },
+    ]
+
+    const sortingOptions = [
+        { value: '최신순', label: '최신순' },
+        { value: '인기순', label: '인기순' },
     ]
 
     const ExceptRegionOptions = ['공지', '자유', '리뷰']
 
     const filteredGroups = postData.filter((post) => {
-        // 전체 카테고리 선택시 모든 포스트 표시
+        // 전체 카테고리 선택 시 모든 포스트 표시
         if (selectedCategory === '전체') {return true};
         
-        // 지역 카테고리 선택시 상세 지역 필터링
+        // 지역 카테고리 선택 시 상세 지역 필터링
         if (selectedCategory === '지역') {
             // ExceptRegionOptions에 포함되지 않은 카테고리만 표시
             if (selectedDetailCategory === '지역 전체') {
@@ -55,44 +80,69 @@ const PostListPage = () => {
         // 그 외 카테고리는 선택된 카테고리만 필터링
         return post.category === selectedCategory;
     })
+
     const notice = postData.filter(
         (post) => post.category === '공지',
     )
 
+    // 정렬 방식
+    const sortedPosts = useMemo(() => {
+        const sorted = [...filteredGroups];
+        
+        if (sortType === '최신순') {
+            return sorted.sort((a, b) => {
+                return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime();
+            });
+        } 
+            return sorted.sort((a, b) => b.likes - a.likes);
+        
+    }, [filteredGroups, sortType]);
+
     return (
         <div>
-            {/* 상단 고정된 공지사항 포스트 리스트 */}
-            {notice.map((post) => (
-            <PostItem key={post.id} post={post} />
-          ))}
-            {/* 카테고리 1 : '지역' / 카테고리 2 : '지역명' 선택 시 상단 공지 포스트 대신 해당 지역 캡션 표시 예정 */}
-            {/* <div
-            className='h-[12rem] w-full bg-cover bg-center text-black text-2xl font-bold flex items-center justify-center p-8'
-            style={{ backgroundImage: `url('src/assets/jpg/postThumbnail.jpg')` }}>
-                <div className='bg-white opacity-80 rounded-3xl p-4'>지역 정보 캡션 - 서울</div>
-            </div> */}
+            { notice.map((post) => (
+                <PostItem key={post.id} post={post} />
+              )) }
 
             {/* 카테고리 선택 드롭다운 */}
-            <div className='flex gap-4 mt-10'>
-                <div className='h-[40px]  relative z-1000'>
+            <div className='flex justify-between mt-10'>
+                <div className='flex'>
+                <div className='h-[40px] relative z-1000 mr-4'>
                     <DropdownSelector
                     options={options}
-                    defaultValue='전체'
+                    defaultValue={selectedCategory}
                     onChange={handleRegionCategory}
                     />
                 </div>
-                {isDetailCategoryOpen &&(<RegionDropdown
-                            isVisible={selectedCategory === '지역'}
-                            options={regionOptions}
-                            defaultValue={selectedDetailCategory}
-                            onChange={setSelectedDetailCategory}
-                />)}
+                {/* 카테고리 '지역' 선택 시 표시되는 상세 지역 선택 드롭다운 */}
+                {isDetailCategoryOpen && (
+                <RegionDropdown
+                    options={regionOptions}
+                    defaultValue={selectedDetailCategory}
+                    onChange={setSelectedDetailCategory}
+                />
+    )}
+                </div>
+                <div className='h-[40px] relative z-1000'>
+                    <DropdownSelector
+                        options={sortingOptions}
+                        defaultValue='최신순'
+                        onChange={setSortType}
+                    />
+                </div>
+                
         </div>
         <div className='flex flex-col  my-8 mx-12 '>
             {/* 선택한 카테고리를 기반으로 한 포스트 아이템 리스트 */}
-          {filteredGroups.map((post) => (
-            <PostItem key={post.id} post={post} />
-          ))}
+            {filteredGroups.length === 0 ? (
+                <div className='flex justify-center items-center h-32 text-darkGray'>
+                    해당하는 게시물이 없습니다.
+                </div>
+            ) : (
+                sortedPosts.map((post) => (
+                    <PostItem key={post.id} post={post} />
+                ))
+            )}
         </div>
         </div>
     );
