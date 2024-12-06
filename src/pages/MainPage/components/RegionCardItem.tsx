823afef9876bd05adcd1cb19607b486af93b0c68
jiwonkim <kiwi_dev_@naver.com>
2024-12-07 04:04:02 +0900

    Feat: 메인, 통합게시판, 검색페이지 포스트맨 api 사용

--- src/pages/MainPage/components/RegionCardItem.tsx
+++ src/pages/MainPage/components/RegionCardItem.tsx
@@ -2,17 +2,55 @@ import { Link } from 'react-router-dom'
 import { regionData } from '@/temporaryData/regionData'
 import NextArrowIcon from '@/assets/svg/nextArrow.svg?react'
 import PrevArrowIcon from '@/assets/svg/prevArrow.svg?react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { AllPostData } from '@/typings/post'
+import axios from 'axios'
+import { regionImages } from '@/temporaryData/regionImages'
 
 const RegionCardItem = () => {
   // 현재 보여지는 지역 카드 인덱스 상태
   const [startIndex, setStartIndex] = useState(0)
+  const [regionCategory, setRegionCategory] = useState<AllPostData[]>([])
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [error, setError] = useState<string | null>(null)
+
+  // 지역 카드 표시 위한 카테고리 조회
+  useEffect(() => {
+    let mounted = true;
+    
+    const getCategory = async () => {
+      if (!mounted) {return};
+      setIsLoading(true)
+      setError(null)
+      try {
+        // 목 서버, 실제 서버로 수정 필요
+        const response = await axios.get('https://83c7c11d-0a32-4b7b-9db8-6f828abf0474.mock.pstmn.io/api/v1/category')
+        
+        if(!response.data) {
+          throw new Error('데이터 형식이 올바르지 않습니다.');
+        }
+        if (mounted) {
+          setRegionCategory(response.data)
+        }
+
+      } catch (error) {
+        if (mounted) {
+        console.error('카테고리 조회 실패:', error)
+        setError('카테고리 조회 실패')
+        }
+      } finally {
+        if (mounted) {
+        setIsLoading(false)
+        }
+      }
+    }
+    getCategory();
+  }, [])
 
   const itemsPerPage = 4
 
   // 지역 카드 - 다음 페이지 핸들러
   const nextBtnHandler = () => {
-    console.log('regionCardHandler')
     if (startIndex + itemsPerPage < regionData.length) {
       setStartIndex(startIndex + itemsPerPage)
     } else {
@@ -22,7 +60,6 @@ const RegionCardItem = () => {
 
   // 지역 카드 - 이전 페이지 핸들러
   const prevBtnHandler = () => {
-    console.log('prevBtnHandler')
     if (startIndex === 0) {
       // 첫 페이지에서 이전 버튼 클릭 시 마지막 페이지로 이동
       const lastPageStart =
@@ -35,7 +72,8 @@ const RegionCardItem = () => {
   }
 
   // 현재 보여줄 데이터 조절
-  const currentItems = regionData.slice(startIndex, startIndex + itemsPerPage)
+  // const currentItems = regionData.slice(startIndex, startIndex + itemsPerPage)
+  const currentItemsInAPI = regionCategory.slice(startIndex, startIndex + itemsPerPage)
 
   return (
     <div className='flex flex-row justify-center items-center my-8 w-full'>
@@ -44,11 +82,11 @@ const RegionCardItem = () => {
         className='w-6 h-6 mr-10'
       />
       <div className='flex flex-row space-x-10'>
-        {currentItems.map((region) => (
+        {currentItemsInAPI.map((region) => (
           <Link
             key={region.id}
             to={`/postlist/region/${encodeURIComponent(region.name)}`}
-            style={{ backgroundImage: `url(${region.thumbnail})` }}
+            style={{ backgroundImage: `url(${regionImages[region.name as keyof typeof regionImages]})` }}
             className='font-bold text-xl w-44 h-56 text-white rounded-xl flex items-end justify-center py-4 bg-cover bg-center bg-no-repeat relative'
           >
             {/* 배경 이미지 위에 어두운 레이어 추가 */}
