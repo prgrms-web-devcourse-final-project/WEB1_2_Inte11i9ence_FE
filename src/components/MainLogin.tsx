554dd7b7a422c3bc8310a3190c2b2e4e427e10f6
ahcgnoej <137070410+ahcgnoej@users.noreply.github.com>
2024-12-08 00:46:55 +0900

    Chore: useEffect 추가

--- src/components/MainLogin.tsx
+++ src/components/MainLogin.tsx
@@ -1,4 +1,4 @@
-import React from 'react' // import axios from 'axios'
+import React, { useEffect } from 'react' // import axios from 'axios'
 import Google from '../assets/png/Google.png'
 import Naver from '../assets/png/Naver.png'
 
