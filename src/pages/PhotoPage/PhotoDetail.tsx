f4803e7702096bab6577d81fcfcf481b59974bd5
ahcgnoej <137070410+ahcgnoej@users.noreply.github.com>
2025-01-18 18:18:04 +0900

    Feat: aos 스크롤 애니메이션 라이브러리 사용

--- src/pages/PhotoPage/PhotoDetail.tsx
+++ src/pages/PhotoPage/PhotoDetail.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import defaultProfileImage from '@assets/png/default-profile-2.png'
 import NextIcon from '@assets/svg/nextArrow.svg?react'
 import PrevIcon from '@assets/svg/prevArrow.svg?react'
-import VoteResults from './vote'
+// import VoteResults from './vote'
 import PinIcon from '@assets/svg/Pin.svg?react'
 import axios from 'axios'
 import { Auth, PhotoDetailProp } from '@/typings/photo'
@@ -23,7 +23,7 @@ const PhotoDetail = ({
   refreshPosts,
 }: PhotoDetailProps) => {
   const [currentIndex, setCurrentIndex] = useState(0)
-  const [hasVoted, setHasVoted] = useState(false)
+  // const [hasVoted, setHasVoted] = useState(false)
   const [postDetail, setPostDetail] = useState<PhotoDetailProp>()
   const token = localStorage.getItem('access_token')
   const { profile } = useProfile(token || '')
@@ -63,10 +63,10 @@ const PhotoDetail = ({
     )
   }
 
-  const handleVote = () => {
-    setHasVoted(true)
-    setCurrentIndex(0)
-  }
+  // const handleVote = () => {
+  //   setHasVoted(true)
+  //   setCurrentIndex(0)
+  // }
 
   const handleDelete = async () => {
     const token = localStorage.getItem('access_token')
@@ -101,7 +101,7 @@ const PhotoDetail = ({
     <div>
       <div className='fixed inset-0 bg-black bg-opacity-90 flex items-center justify-center z-50'>
         <div
-          className='bg-white p-6 rounded-lg shadow-lg w-[90%] sm:w-[35%] lg:w-[25%] relative'
+          className='flex flex-col bg-white p-6 rounded-lg shadow-lg w-[80%] sm:w-[70%] lg:w-[40%] relative h-[90%] justify-between'
           onClick={(e) => e.stopPropagation()}
         >
           <div className='flex justify-center items-center mb-4 gap-2'>
@@ -133,7 +133,7 @@ const PhotoDetail = ({
             )}
           </div>
 
-          <div className='h-[40vh] relative flex items-center justify-center'>
+          <div className='h-[50vh] relative flex items-center justify-center'>
             <button
               onClick={handlePrev}
               className='absolute left-2 top-1/2 transform -translate-y-1/2 bg-gray-700 text-white rounded-full w-8 h-8 flex items-center justify-center z-10'
@@ -158,39 +158,16 @@ const PhotoDetail = ({
               />
             </button>
           </div>
-          <p className='font-bold text-sm text-black mt-4'>
-            {postDetail?.content}
-          </p>
-          {/* 투표 UI */}
-          <div className='mt-4  p-2'>
-            {!hasVoted ? (
-              // 투표 전 UI
-              <div className='flex flex-col border-t pt-2 items-center gap-3'>
-                <div className='flex gap-4 pt-4 items-center'>
-                  <button
-                    className='bg-darkBlue text-white py-2 px-2 text-sm rounded-lg hover:bg-blue-600'
-                    onClick={handleVote}
-                  >
-                    첫 번째 사진
-                  </button>
-                  <p className='font-bold text-lg'>VS</p>
-                  <button
-                    className='bg-darkBlue text-white py-2 px-2 text-sm rounded-lg hover:bg-blue-600'
-                    onClick={handleVote}
-                  >
-                    두 번째 사진
-                  </button>
-                </div>
-              </div>
-            ) : (
-              // 투표 후 UI
-              <VoteResults />
-            )}
+          <div className='w-full'>
+            <p className='font-bold text-sm text-black mt-4'>
+              {postDetail?.content}
+            </p>
           </div>
-          <div className='flex w-full justify-center'>
+
+          <div className='flex w-full justify-center mt-auto'>
             <button
               onClick={onClose}
-              className='mt-4 mx-8'
+              className='mx-8 text-sm'
             >
               닫기
             </button>
--- src/pages/PhotoPage/PhotoPage.tsx
+++ src/pages/PhotoPage/PhotoPage.tsx
@@ -1,11 +1,12 @@
 import { useState, useEffect } from 'react'
 import defaultProfileImage from '@assets/png/default-profile-2.png'
-import PlusIcon from '@assets/svg/Plus.svg?react'
 import { Link } from 'react-router-dom'
 import PhotoDetail from './PhotoDetail'
 import axios from 'axios'
 import formatTime from '@/utils/formatTime'
 import { PhotoList, Auth } from '@/typings/photo'
+import 'aos/dist/aos.css'
+import AOS from 'aos'
 
 const PhotoPage = () => {
   const [postList, setPostList] = useState<PhotoList>()
@@ -13,6 +14,14 @@ const PhotoPage = () => {
   const [selectedPost, setSelectedPost] = useState<number>()
   const [author, setAuthor] = useState<Auth>()
   const token = localStorage.getItem('access_token')
+
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      easing: 'ease-in-out',
+      once: true,
+    })
+  }, [])
 
   const fetchPosts = async () => {
     try {
@@ -35,18 +44,189 @@ const PhotoPage = () => {
   }, [])
 
   return (
-    <div className='flex-col flex '>
-      <div className='flex y-[20px] mx-[70px] bg-white items-center justify-between'>
-        <Link to={'/photo/add'}>
-          <button>
-            <PlusIcon
-              width={20}
-              height={20}
-            />
-          </button>
-        </Link>
+    <div className='flex-col flex mt-[-50px]'>
+      {/* 소개 섹션 */}
+      <div
+        className='flex-col flex bg-black'
+        style={{
+          width: '100vw',
+          marginLeft: 'calc(-50vw + 50%)',
+        }}
+      >
+        <section
+          className='text-white py-60 bg-cover bg-center relative'
+          style={{
+            backgroundImage:
+              "url('https://images.unsplash.com/photo-1581930385341-ab81d2f8f09b?q=80&w=1776&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')",
+            backgroundAttachment: 'fixed',
+            backgroundSize: 'cover',
+            width: '100vw',
+            marginLeft: 'calc(-50vw + 50%)',
+          }}
+        >
+          <div
+            className='absolute inset-0 bg-black bg-opacity-40 z-0'
+            style={{ pointerEvents: 'none' }}
+          ></div>
+          <div
+            className='max-w-full mx-auto text-end px-[5%] relative z-10'
+            data-aos='fade-up'
+            data-aos-delay='400'
+            data-aos-duration='1000'
+            style={{ transform: 'translateY(-120%)' }}
+          >
+            <p className='text-5xl font-bold leading-relaxed p-4 rounded-lg'>
+              여행은 잘 다녀오셨나요?
+            </p>
+          </div>
+          <img
+            id='plane'
+            src='https://cdn-icons-png.flaticon.com/128/7893/7893974.png'
+            alt='Airplane'
+            style={{
+              position: 'absolute',
+              top: '10%',
+              left: '0',
+              zIndex: 20,
+              width: '80px',
+              height: 'auto',
+              animation: 'flyPlane 2s ease-out forwards',
+            }}
+          />
+          <style>
+            {`
+          @keyframes flyPlane {
+            0% {
+              transform: translateX(0) rotate(0deg);
+              opacity: 1;
+            }
+            70% {
+              transform: translateX(50vw) translateY(-10px) rotate(10deg);
+              opacity: 0.8;
+            }
+            100% {
+              transform: translateX(70vw) translateY(-10px) rotate(20deg);
+              opacity: 0;
+            }
+          }
+        `}
+          </style>
+        </section>
+        <section
+          className='text-white py-40 bg-black relative'
+          style={{
+            width: '100vw',
+            marginLeft: 'calc(-50vw + 50%)',
+          }}
+          data-aos='fade-up'
+          data-aos-delay='600'
+        >
+          <div className='absolute inset-0 z-0'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              viewBox='0 0 200 200'
+              className='w-full h-full opacity-20'
+              style={{
+                animation: 'svgAnimation 3s ease-in-out infinite',
+              }}
+            >
+              <circle
+                cx='100'
+                cy='100'
+                r='50'
+                fill='white'
+              />
+              <circle
+                cx='60'
+                cy='60'
+                r='30'
+                fill='lightblue'
+              />
+            </svg>
+          </div>
+          <div className='max-w-4xl mx-auto text-center relative z-10'>
+            <p className='text-3xl font-semibold leading-relaxed'>
+              그곳에서 찍은 수많은 사진들 중,{' '}
+              <span className='text-lightBlue'>단 하나의 특별한 순간이</span>
+              있을 거예요.
+            </p>
+          </div>
+        </section>
+        <section
+          className='text-white bg-black relative'
+          style={{
+            width: '100vw',
+            marginLeft: 'calc(-50vw + 50%)',
+          }}
+          data-aos='fade-up'
+          data-aos-delay='1000'
+        >
+          <div className='max-w-4xl mx-auto pb-24 text-center relative z-10'>
+            <p className='text-3xl font-semibold leading-relaxed'>
+              어쩌면 그 순간은{' '}
+              <span className='text-lightBlue'>아무도 모르는</span> 당신만의{' '}
+              <span className='text-lightBlue'>작은 인생샷</span>일지도 모르죠.
+            </p>
+          </div>
+        </section>
+        <section
+          className='text-white py-20 bg-gradient-to-t from-black to-gray-700'
+          style={{
+            width: '100vw',
+            marginLeft: 'calc(-50vw + 50%)',
+            overflow: 'hidden',
+          }}
+          data-aos='slide-right'
+          data-aos-delay='1200'
+        >
+          <div className='relative max-w-4xl mx-auto text-center z-10'>
+            <p className='text-3xl font-semibold leading-relaxed'>
+              여행 중 마주친 그 소중한 순간을 이곳에서{' '}
+              <span className='text-lightBlue'>다른 사람들과 함께 나누며,</span>
+              <p> 그 기억을 더욱 빛나게 해보세요.</p>
+            </p>
+          </div>
+        </section>
+        <section
+          className='text-white pb-20 bg-black'
+          style={{
+            width: '100vw',
+            marginLeft: 'calc(-50vw + 50%)',
+          }}
+          data-aos='zoom-out'
+          data-aos-delay='800'
+        >
+          <div className='max-w-4xl mx-auto text-center'>
+            <div className='space-x-8'>
+              <Link
+                to={'/photo/add'}
+                className='inline-block px-8 py-3 text-lg font-semibold bg-transparent border-2 border-white text-white rounded-lg hover:bg-white hover:text-black transition-all'
+              >
+                사진 업로드 하기
+              </Link>
+              <a
+                href='#'
+                onClick={(e) => {
+                  e.preventDefault()
+                  const targetElement = document.getElementById('photo-gallery')
+
+                  if (targetElement) {
+                    targetElement.scrollIntoView({ behavior: 'smooth' })
+                  }
+                }}
+                className='inline-block px-8 py-3 text-lg font-semibold bg-transparent border-2 border-white text-white rounded-lg hover:bg-white hover:text-black transition-all'
+              >
+                사진 게시글 보기
+              </a>
+            </div>
+          </div>
+        </section>
       </div>
-      <div className='flex flex-wrap mt-4 h-auto w-full justify-center gap-y-12 gap-x-4'>
+      {/* 게시글 목록*/}
+      <div
+        id='photo-gallery'
+        className='flex flex-wrap pt-20  h-auto w-full justify-center gap-y-12 gap-x-4'
+      >
         {postList?.selectPosts.length === 0 ? (
           <p className='text-center text-darkGray '>게시글이 없습니다.</p>
         ) : (
@@ -59,6 +239,7 @@ const PhotoPage = () => {
                 setIsPhotoDetailOpen(true)
               }}
               className='flex flex-col justify-between p-4 w-[45%] sm:w-[25%] lg:w-[20%] mx-2 bg-white shadow-lg rounded-lg border border-lightGray transition-transform hover:scale-105 hover:shadow-xl gap-4 aspect-[4/5]'
+              data-aos='zoom-in'
             >
               <div className='flex flex-col gap-4'>
                 <div className='flex items-center justify-between'>
